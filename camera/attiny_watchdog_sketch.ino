7be18b23b4c27d2962887c13ffb40ebfb6c9cbf1
Dave Jacobowitz <david.jacobowitz@gmail.com>
2017-08-28 00:04:47 +0000

    updated for multi-pulse shutdown

--- camera/attiny_watchdog_sketch.ino
+++ camera/attiny_watchdog_sketch.ino
@@ -9,6 +9,7 @@ const uint8_t DEBUG2_PIN = 4;
 
 // #define DEBUG
 
+
 #ifdef DEBUG
 const uint32_t NIGHT_DURATION_SECONDS    = 30;
 const uint32_t HOLD_OFF_DURATION_SECONDS = 30;
@@ -16,10 +17,10 @@ const uint32_t WATCHDOG_TIMEOUT_SECONDS  = 30;
 const uint32_t SHUTDOWN_DELAY_SECONDS    = 15;
 
 #else
-const uint32_t NIGHT_DURATION_SECONDS    = 10 * 60 * 60;
+const uint32_t NIGHT_DURATION_SECONDS    = 1 * 60 * 60;
 const uint32_t HOLD_OFF_DURATION_SECONDS = 15 * 60;
 const uint32_t WATCHDOG_TIMEOUT_SECONDS  = 2 * 60;
-const uint32_t SHUTDOWN_DELAY_SECONDS    = 60;
+const uint32_t SHUTDOWN_DELAY_SECONDS    = 3 * 60;
 #endif
 
 
@@ -27,7 +28,7 @@ typedef enum modes_t {
   mode_normal_on,
   mode_night_pre_off,
   mode_night_off,
-  mode_emergency_off,
+  mode_watchdog_off,
 } modes_t;
 
 
@@ -53,18 +54,43 @@ void setup() {
     current_mode = mode_normal_on; 
 }
 
+bool got_pulse() {
+    static bool last_bb = false;
+    bool bb = digitalRead(PULSE_PIN);
+    bool rv = !bb && last_bb;
+    last_bb = bb;
+    return rv;  
+}
+
+bool got_shutdown_request() {
+  static int  p_count = 0;
+  static int  no_p_count = 0;
+  static bool last_reading = false;
+  bool current_reading = digitalRead(DAYTIME_PIN);
+  bool pulse = !current_reading && last_reading;
+  if (pulse) {
+    p_count += 1;
+    no_p_count = 0;
+  } else {
+    no_p_count += 1;
+  }
+  if (no_p_count > 10) p_count = 0;
+  last_reading = current_reading;
+  return (p_count == 3);
+}
+
 void loop() {
     switch (current_mode) {
         case mode_normal_on:
             digitalWrite(DEBUG1_PIN, LOW);
             digitalWrite(DEBUG2_PIN, LOW);
             if (time_unpinged > WATCHDOG_TIMEOUT_SECONDS) {
-              current_mode = mode_emergency_off;
+              current_mode = mode_watchdog_off;
               time_off = 0;
               time_unpinged = 0;
             }
             
-            if (!digitalRead(DAYTIME_PIN)) {
+            if (got_shutdown_request()) {
               current_mode = mode_night_pre_off;
               time_pre_off = 0;
             }
@@ -74,7 +100,11 @@ void loop() {
             digitalWrite(DEBUG1_PIN, HIGH);
             digitalWrite(DEBUG2_PIN, LOW);
 
-            if (time_pre_off > SHUTDOWN_DELAY_SECONDS) {
+            if (got_pulse()) {
+              time_pre_off = 0;
+              time_unpinged = 0;
+              current_mode = mode_normal_on;
+            } else if (time_pre_off > SHUTDOWN_DELAY_SECONDS) {
               current_mode = mode_night_off;
               time_pre_off = 0;
             } else {
@@ -86,7 +116,9 @@ void loop() {
             digitalWrite(DEBUG1_PIN, LOW);
             digitalWrite(DEBUG2_PIN, HIGH);
 
-            if (time_off > NIGHT_DURATION_SECONDS) {
+            if ((time_off > NIGHT_DURATION_SECONDS) || got_pulse()
+            ) {
+              time_unpinged = 0;
               current_mode = mode_normal_on;
               time_off = 0;
             } else {
@@ -94,11 +126,11 @@ void loop() {
             }
             break;
             
-        case mode_emergency_off:
+        case mode_watchdog_off:
             digitalWrite(DEBUG1_PIN, HIGH);
             digitalWrite(DEBUG2_PIN, HIGH);
 
-            if (time_off > HOLD_OFF_DURATION_SECONDS) {
+            if ((time_off > HOLD_OFF_DURATION_SECONDS) || got_pulse()) {
               time_off = 0;
               time_unpinged = 0;
               current_mode = mode_normal_on;
@@ -106,15 +138,15 @@ void loop() {
               time_off += 1;
             }
     }
-    
-    if (!digitalRead(PULSE_PIN)) {
+
+    if (got_pulse()) {
       time_unpinged = 0;
     } else {
       time_unpinged += 1;
     }
 
     bool off = (current_mode == mode_night_off) ||
-               (current_mode == mode_emergency_off);
+               (current_mode == mode_watchdog_off);
     digitalWrite(OFF_PIN, off);
     delay(1000);
 
