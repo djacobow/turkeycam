5d1f1c95e56c924f3b5c339cb17c2e5565833a63
Dave Jacobowitz <david.jacobowitz@gmail.com>
2017-09-13 16:06:07 -0700

    refactor and change detection

---
+++ camera/Wdog.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+
+import time
+import datetime
+import RPi.GPIO as GPIO
+import subprocess
+import re
+
+class Wdog:
+    def __init__(self,icfg):
+        self.cfg = icfg
+        self.last_heartbeat = datetime.datetime.now()
+        self.batt_nok_count = 0
+        self.beat_count = 0
+
+    def setup(self):
+
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.cfg['heartbeat_pin'], GPIO.OUT, initial = GPIO.HIGH)
+        GPIO.setup(self.cfg['shutdown_pin'], GPIO.OUT, initial = GPIO.HIGH)
+        GPIO.setup(self.cfg['lowbatt_pin'], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    def signalShutdownRequest(self):
+        # not one, but several pulses are required to get watchdog
+        # to initiate power-off 
+        GPIO.output(self.cfg['heartbeat_pin'], GPIO.HIGH)
+        for x in range(4):
+            print("Pulse " + str(x))
+            GPIO.output(self.cfg['shutdown_pin'], GPIO.HIGH)
+            GPIO.output(self.cfg['heartbeat_pin'], GPIO.LOW)
+            time.sleep(2)
+            GPIO.output(self.cfg['shutdown_pin'], GPIO.LOW)
+            GPIO.output(self.cfg['heartbeat_pin'], GPIO.HIGH)
+            time.sleep(2)
+
+    def unsetup(self):
+        GPIO.output(self.cfg['heartbeat_pin'], GPIO.HIGH)
+        GPIO.output(self.cfg['shutdown_pin'], GPIO.HIGH)
+
+    def battIsOK(self):
+        batt_ok = GPIO.input(self.cfg['lowbatt_pin'])
+        if not batt_ok:
+            batt_nok_count += 1
+        else:
+            batt_nok_count = 0
+        return batt_nok_count < self.cfg['max_lowbatt_before_shutdown']
+
+    def beatHeart(self):
+        now = datetime.datetime.now()
+        if now - self.last_heartbeat > datetime.timedelta(seconds=self.cfg['heartbeat_period']):
+            self.last_heartbeat = now
+            GPIO.output(self.cfg['heartbeat_pin'], GPIO.LOW)
+            self.beat_count = 0
+        elif self.beat_count == self.cfg['heartbeat_ticks']:
+            self.beat_count = 0
+            GPIO.output(self.cfg['heartbeat_pin'], GPIO.HIGH)
+        else:
+            self.beat_count += 1
+
+    def shutdown(self):
+
+        print('Shutting down in ' + str(self.cfg['shutdown_delay']) + ' seconds!')
+        st = datetime.datetime.now()
+        while (datetime.datetime.now() - st).seconds < self.cfg['shutdown_delay']:
+            print(datetime.datetime.now() - st)
+            GPIO.output(self.cfg['heartbeat_pin'], GPIO.LOW)
+            time.sleep(2)
+            GPIO.output(self.cfg['heartbeat_pin'], GPIO.HIGH)
+            time.sleep(2)
+
+        print('Telling watchdog to turn off power.')
+        self.signalShutdownRequest()
+        if True:
+            print('Shutting down.')
+            process = subprocess.Popen(self.cfg['shutdown_cmd'].split(), stdout=subprocess.PIPE)
+            output = process.communicate()[0]
+            print(output)
+
+    def wait_for_time_sync(self):
+        ready = False
+        while not ready:
+            process = subprocess.Popen(self.cfg['datetimecmd'].split(), stdout=subprocess.PIPE)
+            output = [x.decode('ascii') for x in process.communicate()[0].splitlines() ]
+
+            for line in output:
+                if re.match(r'NTP synchronized: yes',line):
+                    print('Time synchronized.')
+                    ready = True
+            if not ready:
+                print('Waiting for clock to be ready.')
+                time.sleep(5)
+
+
---
+++ camera/capture2.py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python3
+
+import sys
+import time
+import io
+import datetime
+import pytz
+import base64
+import json
+import picamera
+from skimage.measure import compare_ssim as ssim
+import numpy as np
+from PIL import Image
+import requests
+from Wdog import Wdog
+from Daylight import Daylight
+
+last_idata = None
+trailing_average_sameness = 1
+
+with open('secret_code.json') as fh:
+    secret = json.load(fh)
+
+
+cfg = {
+    'wdog': {
+        'shutdown_pin': 13,
+        'heartbeat_pin': 11,
+        'lowbatt_pin': 15,
+        'shutdown_cmd': '/usr/bin/sudo /sbin/shutdown -h now',
+        'max_lowbatt_before_shutdown': 4,
+        'heartbeat_period': 10,
+        'shutdown_delay': 90,
+        'heartbeat_ticks': 10,
+        'datetimecmd': '/usr/bin/timedatectl',
+    },
+    'use_pil': False,
+    'token': secret['token'],
+    'url': 'https://skunkworks.lbl.gov/turkeycam/newimage',
+    'city': 'San Francisco',
+    #'tzname': 'US/Pacific',
+    'picture_period': 5,
+    'tick_length': 0.5,
+    'cam_params': {
+        'resolution': (2560, 2048),
+        #'resolution': (3280, 2464),  # max native res for pi camera2
+        # auto iso
+        #'iso': 100,
+        'vflip': False,
+        'image_effect': 'denoise',
+    },
+}
+
+
+def myIP():
+    try:
+        # return requests.get('https://api.ipify.org/?format=json').json()['ip']
+        return requests.get('https://ipinfo.io').json()['ip']
+    except:
+        return 'dunno'
+
+
+def captureToBytesRGB():
+    stream = None
+    image = None
+
+    with picamera.PiCamera() as camera:
+        for param in cfg['cam_params']:
+            setattr(camera,param,cfg['cam_params'][param])
+        stream = io.BytesIO()
+        time.sleep(0.5) # this is for the camera to adjust its exposure
+        print('Capture image')
+        camera.capture(stream, format='rgb')
+        width = cfg['cam_params']['resolution'][0]
+        height = cfg['cam_params']['resolution'][1]
+        stream.seek(0)
+        image = np.frombuffer(stream.getvalue(), dtype=np.uint8).reshape(height, width, 3)
+        stream.seek(0)
+    return {'npimage': image, 'stream': stream}
+        
+
+
+def compareImages(i0,i1):
+    # this basically resizes a 2d array, to make a pic smaller
+    def rebin(a, shape):
+        sh = shape[0],a.shape[0]//shape[0],shape[1],a.shape[1]//shape[1]
+        return a.reshape(sh).mean(-1).mean(1)
+
+    if not i0 or not i1:
+        return None
+
+    # convert to grayscale
+    gs0 = np.mean(i0['npimage'], axis=2)
+    gs1 = np.mean(i1['npimage'], axis=2)
+    # shrink to make calc faster
+    newshape = [ int(x/4) for x in gs0.shape ]
+
+    sm0 = rebin(gs0,newshape)
+    sm1 = rebin(gs1,newshape)
+
+    s = ssim(sm0, sm1)
+    print('Images are ' + str(s) + ' similar.')
+    return s
+
+def takePhotoAndMaybeUpload(ip):
+    global last_idata
+    global trailing_average_sameness
+
+    try:
+        idata = captureToBytesRGB()
+        sameness = compareImages(idata, last_idata)
+
+        if sameness is not None:
+            trailing_average_sameness += -0.1 * trailing_average_sameness + 0.1 * sameness
+            if sameness < 0.90 * trailing_average_sameness:
+                res = uploadImage(idata,ip)
+                print(res)
+
+        last_idata = idata
+
+    except Exception as e:
+        print('well, that didn\'t work')
+        print(e)
+
+
+def uploadImage(img, ip = None):
+    now = datetime.datetime.now()
+
+    pilimg = Image.fromarray(img['npimage'],'RGB')
+    fstr   = io.BytesIO()
+    pilimg.save(fstr,format='jpeg')
+    fstr.seek(0)
+
+    data = {
+        'camera_name': secret.get('camera_name',''),
+        'token': cfg['token'],
+        'source': 'turkeyCam',
+        'date': now.isoformat(),
+        'source_ip': ip,
+        'image_jpeg': base64.b64encode(fstr.getvalue()).decode('utf-8'),
+    }
+    return requests.post(cfg['url'], data = data, timeout=60)
+ 
+
+
+
+
+def mymain(wdog):
+
+    wdog.setup()
+    wdog.wait_for_time_sync()
+
+    day = Daylight(cfg['city'])
+
+    last_shot      = pytz.timezone('utc').localize(datetime.datetime.now())
+
+    ip = myIP()
+    count = 0
+    running = True;
+    batt_nok_count = 0
+
+    while running:
+        now = pytz.timezone('utc').localize(datetime.datetime.now())
+
+        #if count > 40:
+        if not day.isDaylight():
+            running = False
+            wdog.shutdown()
+
+        if not wdog.battIsOK():
+            wdog.shutown()
+
+        if now - last_shot > datetime.timedelta(seconds=cfg['picture_period']):
+            last_shot = now
+            takePhotoAndMaybeUpload(ip)
+
+        wdog.beatHeart()
+
+        time.sleep(cfg['tick_length']);
+        count += 1
+  
+
+if __name__ == '__main__':
+    wdog = Wdog(cfg['wdog'])
+    try:
+        mymain(wdog)
+    except Exception as e:
+        print('Whoops!')
+        print(e)
+    
+        wdog.unsetup()
