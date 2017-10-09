680b6ed459c5d84e7dea7b9439498800813a6208
Dave Jacobowitz <dgj@lbl.gov>
2017-10-09 12:40:07 -0700

    format not to say turkeys everywhere and to show results more clearly

--- server/static/turkeys.js
+++ server/static/turkeys.js
@@ -1,4 +1,26 @@
 
+function showJS(jserr, jsdata, jsurl) {
+    var labels = Object.keys(jsdata);
+    var ul = document.createElement('ul');
+    for (var j=0;j<labels.length;j++) {
+        var label = labels[j];
+        var ldata = jsdata[label];
+        var li = document.createElement('li');
+        ul.appendChild(li);
+        var value = '';
+        if (label === 'aws_results') {
+            /* jshint loopfunc: true */
+            value = ldata.Labels.map(function(x) { 
+                var y = Math.floor(parseFloat(x.Confidence)+0.5); 
+                return x.Name + ' (' + y + ')'; }).join(', ');
+        } else {
+            value = JSON.stringify(ldata);
+        }
+        li.innerText = label + ': ' + value;
+    }
+    document.getElementById(jsurl).appendChild(ul);
+}
+
 var populateTurkeys = function() {
     var turktable = document.getElementById('turktable');
     removeChildren(turktable);
@@ -16,8 +38,8 @@ var populateTurkeys = function() {
             var isrc = '/turkeycam/turkeys/' + data[date].jpg;
             if (isrc) {
                 var img  = document.createElement('img');
-                img.style.width = '300';
-                img.style.height= '225';
+                img.style.width = '600';
+                img.style.height= '450';
                 img.src = isrc;
                 var a = document.createElement('a');
                 a.href = isrc;
@@ -27,20 +49,17 @@ var populateTurkeys = function() {
                 var row = document.createElement('tr');
                 var imgtd = document.createElement('td');
                 imgtd.appendChild(a);
-                imgtd.style.width = '20%';
+                imgtd.style.width = '50%';
                 row.appendChild(imgtd);
                 if ('json' in data[date]) {
                     var jsrc = '/turkeycam/turkeys/' + data[date].json;
                     var datatd = document.createElement('td');
-                    datatd.style.width = '80%';
+                    datatd.style.width = '50%';
                     var dataspan = document.createElement('span');
                     dataspan.id = jsrc;
                     datatd.appendChild(dataspan);
                     row.appendChild(datatd);
-                    /* jshint loopfunc: true */
-                    getJSON(jsrc, function(jserr,jsdata,jsurl) {
-                        document.getElementById(jsurl).innerText = JSON.stringify(jsdata);
-                    });
+                    getJSON(jsrc, showJS);
                 }
                 turktable.appendChild(row);
             }
